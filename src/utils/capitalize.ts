a384e4ddf7753e89669406a52c01d10971701d83
soonki-98 <alstnsrl98@gmail.com>
2023-02-22 16:09:44 +0900

    feat: 파일 이름에 -가 있을때 분기처리

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,6 +1,9 @@
 const capitalize = (str: string) => {
-  const _str = str;
-  return _str.replace(/^\w/, (c) => c.toUpperCase());
+  const _str = str.split("-");
+  let result = "";
+
+  _str.forEach((s) => (result += s.replace(/^\w/, (c) => c.toUpperCase())));
+  return result;
 };
 
 export default capitalize;
