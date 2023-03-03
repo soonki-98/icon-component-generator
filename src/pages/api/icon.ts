364cc2651b9bf6e154b71db621209c7a83de875c
soonki-98 <alstnsrl98@gmail.com>
2023-03-03 23:47:26 +0900

    feat: 저장 위치 선택 가능하도록 api 수정

--- src/pages/api/icon.ts
+++ src/pages/api/icon.ts
@@ -6,28 +6,44 @@ import { transform } from "@svgr/core";
 import capitalize from "@utils/capitalize";
 import propTypesTemplate from "plugins/propsTypesTemplate";
 
+const checkDuplication = (existComponentList: string[], componentName: string) => {
+  let isDuplicated = false;
+
+  for (const component of existComponentList) {
+    if (capitalize(component.replace(".tsx", "")) === componentName) {
+      isDuplicated = true;
+      return;
+    }
+  }
+
+  return isDuplicated;
+};
+
+/**
+ * @param fileName component name to save.
+ * @param svg icon file
+ * @param saveLocation location that you want to generate icon component
+ */
 const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
   try {
-    const { fileName, svg } = req.body;
-    const defaultPath = [process.cwd(), "src/component/icon"];
+    const { fileName, svg, saveLocation } = req.body;
+
+    const componentRoute = [process.cwd(), saveLocation || "src/component/icon"];
     const componentName = capitalize(fileName);
 
-    const componentFilePath = path.join(...defaultPath, `${componentName}.tsx`);
-    const indexFilePath = path.join(...defaultPath, "index.ts");
+    const componentPath = path.join(...componentRoute, `${componentName}.tsx`);
+    const indexFilePath = path.join(...componentRoute, "index.ts");
+
     const indexFile = fs.readFileSync(indexFilePath, { encoding: "utf-8" });
+    const existComponentList = fs.readdirSync(componentRoute.join("/"));
 
-    const files = fs.readdirSync(defaultPath[0] + "/" + defaultPath[1]);
+    const isDuplicated = checkDuplication(existComponentList, componentName);
 
-    for (const file of files) {
-      if (file.replace(".tsx", "").toLowerCase() === fileName.toLowerCase()) {
-        res.status(400);
-        res.send({
-          status: 400,
-          success: false,
-          message: `${fileName} is already exist`,
-        });
-        return;
-      }
+    if (isDuplicated) {
+      res.status(400).send({
+        success: false,
+        message: `${fileName} is already exist`,
+      });
     }
 
     const result = await transform(
@@ -41,17 +57,15 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<any>) => {
       { componentName }
     );
 
-    fs.writeFileSync(componentFilePath, result);
+    fs.writeFileSync(componentPath, result);
     fs.writeFileSync(indexFilePath, `${indexFile}export { default as ${componentName} } from "./${componentName}"; `);
 
-    res.send({
-      status: 200,
+    res.status(200).send({
       success: true,
       message: `Success to convert ${componentName}.svg to component`,
     });
   } catch (err) {
-    res.send({
-      status: 500,
+    res.status(500).send({
       success: false,
       message: `Failed to convert`,
     });
--- src/utils/uploadSVG.tsx
+++ src/utils/uploadSVG.tsx
@@ -1,20 +1,34 @@
 import axios from "axios";
 
+/**
+ * @param fileName component name to save.
+ * @param svg icon file
+ * @param saveLocation location that you want to generate icon component.
+ *
+ * base location of 'saveLocation' is 'root'.
+ * so if you want to generate svg at 'component/icon', then write 'src/component/icon'
+ *
+ */
+const generateIcon = async (svg: string, fileName: string, saveLocation?: string) => {
+  return await axios.post("/api/icon", {
+    svg,
+    fileName,
+    saveLocation,
+  });
+};
+
 const uploadSVG = async (file: File) => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsText(file, "UTF-8");
 
     reader.onload = async (e) => {
-      await axios
-        .post("/api/icon", {
-          svg: e.target?.result as string,
-          fileName: file.name.replace(".svg", ""),
-        })
-        .then((res) => {
-          resolve(res);
-        })
-        .catch((err) => alert(err.response.data.message));
+      try {
+        const result = await generateIcon(e.target?.result as string, file.name.replace(".svg", ""));
+        resolve(result);
+      } catch (err: any) {
+        reject(err.response.data.message);
+      }
     };
   });
 };
